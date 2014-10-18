12621125eeaa537e55f62879bdc738df2528ac20
Michael Goldman <goldmandrummer@gmail.com>
2014-10-18 15:37:54 -0400

    QR codes in folder && server.js

--- RayGun_Server/server.js
+++ RayGun_Server/server.js
@@ -2,9 +2,56 @@ var app = require('express')();
 var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var db = {
+	funky_test1: {
+		owner: "Michael_Goldman",
+		beacons: [],
+		players: {
+			Michael_Goldman: {
+				isHit: false,
+				score: 0
+			},
+			Andy_Shi: {
+				isHit: false,
+				score: 0
+			},
+			Conlin_Durbin: {
+				isHit: false,
+				score: 0
+			},
+			Kunal_Agrawal: {
+				isHit: false,
+				score: 0
+			}
+		},
+		score: 0
+	}
+};
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.post('/shoot', function(req, res){
+	var game = req.body.game;
+	var player = req.body.player;
+	var victim = req.body.victim;
+
+	if (!db[game].players[victim].isHit) {
+		db[game].players[victim].isHit = true;
+		db[game].players[player].score++;
+		res.json({hit: true});
+	} else {
+		res.json({hit: false});
+	};
+});
+
+app.post('/checkHits', function(req, res){
+	var game = req.body.game;
+	var player = req.body.player;
+
+	if (db[game].players[player].isHit) {
+		res.json({hit: true}));
+		db[game].players[player].isHit = false;
+	} else {
+		res.json({hit:false});
+	};
 });
 
 io.on('connection', function(socket){
