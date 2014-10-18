8e4888e7a70adc6287dce2ddc2a6722a3a5aea75
Andy Shi <shi110@purdue.edu>
2014-10-18 01:38:57 -0400

    Initial Commit
    Initial Commit for iBeacon

---
+++ iBeacon/iBeacon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iBeacon
+//
+//  Created by Andy Shi on 10/18/14.
+//  Copyright (c) 2014 Andy Shi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iBeacon/iBeacon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iBeacon
+//
+//  Created by Andy Shi on 10/18/14.
+//  Copyright (c) 2014 Andy Shi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
